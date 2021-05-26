0aea7e289f15e495a660070f02bd7b6a870003cc
pranta-barua007 <prantabarua247@gmail.com>
2021-05-26 21:43:08 +0600

    final build

--- src/App.js
+++ src/App.js
@@ -1,106 +1,73 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
 import Particles from 'react-particles-js';
-import SMART_BRAIN_API_URL from './api/api';
-
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-import Profile from './components/Profile/Profile';
-import Modal from './components/Modal/Modal';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
 
-const particlesOptions = {
-  //customize to your own liking
-  particles: {
-    number: {
-      value: 30,
-      density: {
-        enable: true,
-        value_area: 800
+
+const particleOptions = {
+    particles: {
+      number: {
+        value: 30,
+        density: {
+          enable: true,
+          value_area: 800
+        }
       }
     }
-  }
 }
+
 
 const initialState = {
   input: '',
   imageUrl: '',
   boxes: [],
   route: 'signin',
-  isProfileOpen: false,
   isSignedIn: false,
   user: {
     id: '',
     name: '',
     email: '',
+    password: '',
     entries: 0,
-    joined: '',
-    age: 0,
-    pet: ''
-  }
+    joined: ''
+  } 
 }
 
 class App extends Component {
-  constructor() {
+  constructor (){
     super();
     this.state = initialState;
   }
 
-  componentDidMount() {
-    const token = window.sessionStorage.getItem('token');
-    if (token) {
-      fetch(`${SMART_BRAIN_API_URL}/signin`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': token
-        }
-      })
-        .then(response => response.json())
-        .then(data => {
-          if (data && data.id) {
-            fetch(`${SMART_BRAIN_API_URL}/profile/${data.id}`, {
-              method: 'GET',
-              headers: {
-                'Content-Type': 'application/json',
-                'Authorization': token
-              }
-            })
-            .then(response => response.json())
-            .then(user => {
-              if (user && user.email) {
-                this.loadUser(user)
-                this.onRouteChange('home');
-              }
-            })
-          }
-        })
-        .catch(console.log)
-    }
-  }
-
   loadUser = (data) => {
     this.setState({user: {
-      id: data.id,
-      name: data.name,
-      email: data.email,
-      entries: data.entries,
-      age: data.age,
-      pet: data.pet,
-      joined: data.joined
-    }})
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+    }
+    });
   }
 
-  calculateFaceLocation = (data) => {
-    const image = document.getElementById('inputimage');
-    const width = Number(image.width);
-    const height = Number(image.height);
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  calculateFaceLocations = (data) => {
     return data.outputs[0].data.regions.map(face => {
-      const clarifaiFace = face.region_info.bounding_box;
+      const clarifaiFace = face.region_info.bounding_box
+    
+      const image = document.getElementById('inputImage');
+      const width = Number(image.width);
+      const height = Number(image.height);
+  
       return {
         leftCol: clarifaiFace.left_col * width,
         topRow: clarifaiFace.top_row * height,
@@ -110,105 +77,80 @@ class App extends Component {
     });
   }
 
+ 
   displayFaceBox = (boxes) => {
-    if(boxes){
-      this.setState({boxes: boxes});
-    }
-  }
-
-  onInputChange = (event) => {
-    this.setState({input: event.target.value});
+    this.setState({boxes: boxes})
   }
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});
-      fetch(`${SMART_BRAIN_API_URL}/imageurl`, {
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': window.sessionStorage.getItem('token')
-        },
-        body: JSON.stringify({
-          input: this.state.input
-        })
+    this.setState({ imageUrl: this.state.input })
+      fetch('https://polar-waters-41859.herokuapp.com/imageurl', {
+          method: 'post',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+              input: this.state.input
+          })
       })
       .then(response => response.json())
-      .then(response => {
-        if (response) {
-          fetch(`${SMART_BRAIN_API_URL}/image`, {
-            method: 'put',
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': window.sessionStorage.getItem('token')
-            },
-            body: JSON.stringify({
-              id: this.state.user.id
-            })
+      .then(response => { 
+        if(response) {
+          fetch('https://polar-waters-41859.herokuapp.com/image', {
+              method: 'put',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({
+                  id: this.state.user.id
+              })
           })
             .then(response => response.json())
             .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count}))
+              this.setState(Object.assign(this.state.user, {entries: count}))
             })
-            .catch(console.log)
-
+          .catch(console.log)  
         }
-        this.displayFaceBox(this.calculateFaceLocation(response))
+        this.displayFaceBox(this.calculateFaceLocations(response))
       })
-      .catch(err => console.log(err));
+      .catch(err => console.log(err))
   }
+
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      return this.setState(initialState)
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
     this.setState({route: route});
   }
 
-  toggleModal = () => {
-    this.setState(state => ({
-      ...state,
-      isProfileOpen: !state.isProfileOpen,
-    }));
-  }
-
-  render() {
-    const { isSignedIn, imageUrl, route, boxes, isProfileOpen, user } = this.state;
+  render(){
+    const { isSignedIn, imageUrl, route, boxes } = this.state;
     return (
       <div className="App">
         <Particles className='particles'
-          params={particlesOptions}
+          params={particleOptions}         
         />
-        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} toggleModal={this.toggleModal}/>
-        {
-          isProfileOpen &&
-          <Modal>
-            <Profile isProfileOpen={isProfileOpen} toggleModal={this.toggleModal} user={user} loadUser={this.loadUser} />
-          </Modal>
-        }
-        { route === 'home'
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home' 
           ? <div>
               <Logo />
-              <Rank
-                name={this.state.user.name}
-                entries={this.state.user.entries}
-              />
-              <ImageLinkForm
+              <Rank name={this.state.user.name} entries={this.state.user.entries}/> 
+              <ImageLinkForm  
                 onInputChange={this.onInputChange}
                 onButtonSubmit={this.onButtonSubmit}
               />
-              <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
+              <FaceRecognition boxes={boxes} imageUrl={imageUrl} /> 
             </div>
           : (
-             route === 'signin'
-             ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-             : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-            )
+            route === 'signin'
+            ?  <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+          ) 
         }
       </div>
     );
+
   }
+  
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
+
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
-  return (
-    <div>
-      <p className='f3'>
-        {'This Magic Brain will detect faces in your pictures. Just provide a image URL, Give it a try.'}
-      </p>
-      <div className='center'>
-        <div className='form center pa4 br3 shadow-5'>
-          <input 
-            className='f4 pa2 w-70 center' 
-            type='text'
-            placeholder='https://someimage.jpg' 
-            onChange={onInputChange}/>
-          <button
-            className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
-            onClick={onButtonSubmit}
-          >Detect</button>
+    return (
+        <div className='ma3 mt0'>
+           <p className='f3'>
+            {'This is a Smart Brain that will detect face in Images, just provide an image URL'}
+           </p>
+           <div className='center'>
+            <div className='center form pa4 br3 shadow-5'>
+              <input className='f4 pa2 w-70 center' type='text' placeholder='https://example-image.jpg'
+                onChange={onInputChange} 
+              />
+              <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit}>
+                Detect
+              </button>
+            </div>
+           </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default ImageLinkForm;
